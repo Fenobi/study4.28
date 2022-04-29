f977643a22e1307d441f8cf68d5ffa7ddc0bbebe
fu-qingyun <1366773912@qq.com>
2022-04-29 10:47:13 +0800

    添加项目文件。

---
+++ fu.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+
+//int check_sys()
+//{
+//	union U
+//	{
+//		char ch;
+//		int i;
+//	}u;
+//	u.i = 1;
+//	return u.ch;
+//}
+//
+//int main()
+//{
+//	int ret = check_sys();
+//
+//	if (ret)
+//	{
+//		printf("Ð¡¶Ë\n");
+//	}
+//	else
+//	{
+//		printf("´ó¶Ë\n");
+//	}
+//	return 0;
+//}
+
+//union Un
+//{
+//	char ch[5];//5
+//	int i;//4
+//};
+//
+//int main()
+//{
+//	union Un u;
+//	printf("%d\n", sizeof(u));//8
+//}
